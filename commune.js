a3d8c40e223a9cb6304fa8684e236031c199cf6a
Emmanuel ROCHET <E149760H@U-INF-J-F113-15.iut-nantes.univ-nantes.prive>
2016-03-24 14:49:40 +0100

    maj index

--- commune.js
+++ commune.js
@@ -1,1 +1,26 @@
-$('#text').focus(function)
+$(document).ready(function(){
+
+
+
+  $('#text').focus(function()
+  {
+    $.ajax({
+      url='https://www.flickr.com/',
+      type:'GET',
+      dataType:'json',
+      data:'commune='+$('#text').val(),
+
+      success: function()
+      })
+    $("#text").autocomplete(
+      {
+        source :
+      }
+    )
+
+      )
+    $()
+
+  })
+
+})
